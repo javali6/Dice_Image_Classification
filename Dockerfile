27858107cb8f1c8405ec525c67a445dfaad141d4
Miłosz Gmerek <milosz.gmerek6@gmail.com>
2024-05-09 18:45:47 +0200

    First fix xd

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,9 @@ FROM pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
 
 WORKDIR /app
 
-COPY requirements.txt /app/requirements.txtgit s
+COPY requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-
-COPY dice_train.py /app/dice_train.py
-COPY utilities.py /app/utilities.py
-COPY data/dice.csv /app/data/dice.csv
+COPY dice_train.py utilities.py data/dice.csv /app/
 
 CMD ["python", "dice_train.py"]
--- dice_train.py
+++ dice_train.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -129,9 +127,6 @@ def main():
             f", Accuracy: {train_accuracy:.2f}%",
             f", Precision: {train_precision:.2f}%",
         )
-        print(
-            f", Accuracy: {train_accuracy:.2f}%", f", Precision: {train_precision:.2f}%"
-        )
         # run["training/precision"].log(train_precision)
         # run["training/accuracy"].log(train_accuracy)
         # run["training/loss"].log(train_loss / len(train_loader))
@@ -158,71 +153,23 @@ def main():
         f"Accuracy on test set: {test_accuracy:.2f}%",
         f"Precision on test set: {test_precision:.2f}%",
     )
-    print(
-        f"Accuracy on test set: {test_accuracy:.2f}%",
-        f"Precision on test set: {test_precision:.2f}%",
-    )
     # run["testing/test_accuracy"] = test_accuracy
     # run["testing/test_precision"] = test_precision
 
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(
-        conf_matrix_train,
-        annot=True,
-        fmt="d",
-        cmap="Blues",
-        xticklabels=np.unique(train_y_true),
-        yticklabels=np.unique(train_y_true),
+    utilities.plot_confusion_matrix(
+        conf_matrix_test,
+        y_true,
+        "Confusion Matrix - Test Set",
+        "confusion_matrix_test.png",
     )
-    plt.xlabel("Predicted Labels")
-    plt.ylabel("True Labels")
-    plt.title("Confusion Matrix - Train Set")
-    plt.savefig("confusion_matrix_train.png")
     # run["training/confusion_matrix"].upload('confusion_matrix_train.png')
 
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(
+    utilities.plot_confusion_matrix(
         conf_matrix_test,
-        annot=True,
-        fmt="d",
-        cmap="Blues",
-        xticklabels=np.unique(y_true),
-        yticklabels=np.unique(y_true),
+        y_true,
+        "Confusion Matrix - Test Set",
+        "confusion_matrix_test.png",
     )
-    plt.xlabel("Predicted Labels")
-    plt.ylabel("True Labels")
-    plt.title("Confusion Matrix - Test Set")
-    plt.savefig("confusion_matrix_test.png")
-    # run["testing/confusion_matrix"].upload('confusion_matrix_test.png')
-
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(
-        conf_matrix_train,
-        annot=True,
-        fmt="d",
-        cmap="Blues",
-        xticklabels=np.unique(train_y_true),
-        yticklabels=np.unique(train_y_true),
-    )
-    plt.xlabel("Predicted Labels")
-    plt.ylabel("True Labels")
-    plt.title("Confusion Matrix - Train Set")
-    plt.savefig("confusion_matrix_train.png")
-    # run["training/confusion_matrix"].upload('confusion_matrix_train.png')
-
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(
-        conf_matrix_test,
-        annot=True,
-        fmt="d",
-        cmap="Blues",
-        xticklabels=np.unique(y_true),
-        yticklabels=np.unique(y_true),
-    )
-    plt.xlabel("Predicted Labels")
-    plt.ylabel("True Labels")
-    plt.title("Confusion Matrix - Test Set")
-    plt.savefig("confusion_matrix_test.png")
     # run["testing/confusion_matrix"].upload('confusion_matrix_test.png')
 
     # Save the model
